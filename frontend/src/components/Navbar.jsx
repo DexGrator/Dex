0ef0512d7e4077636dff2f495810293310f5d49d
tusharpamnani <tusharpamnani1@gmail.com>
2024-09-11 14:59:09 +0530

    giant-swim

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,8 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "../app/ui/buttons";
-import { Menu, X } from "lucide-react";
+import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "../app/ui/sheet";
-import ConnectWalletButton from "./CnnectWalletButton";
 
 const NavLink = ({ href, onClick, children }) => (
   <Link href={href}>
@@ -19,53 +18,71 @@ const Header = () => {
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <div className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <div className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 text-white">
       <div className="container h-20 relative">
         <div className="flex h-full items-center justify-between">
           {/* ================== */}
           {/* DISPLAY THE LOGO  */}
           {/* ================== */}
           <Link href="/">
-            <Image src="/logos.png" width={170} height={100} alt="logo" />
+            <Image
+              src={"/image.png"}
+              width={170}
+              height={100}
+              alt="logo"
+              className="bg-[#021524]"
+            />
           </Link>
           {/* ============================ */}
-          {/* DISPLAY THE MENU FOR DESKTOP */}
+          {/* DISPLAY THE MENU AND BUTTON ON THE RIGHT */}
           {/* ============================ */}
-          <div className="hidden lg:flex gap-10 text-lg font-semibold">
-            <NavLink href="/">Home</NavLink>
-            <NavLink href="/">Swap Tokens</NavLink>
-            <NavLink href="/">Team</NavLink>
-            <NavLink href="/contact">Contact Us</NavLink>
-          </div>
-          <div className="hidden lg:flex gap-6 rounded-full bg-blue-200 px-4 py-2 hover:bg-blue-400 font-bold">
-            <ConnectWalletButton />
-          </div>
-          {/* =========================== */}
-          {/* DISPLAY THE MENU FOR MOBILE */}
-          {/* =========================== */}
-          <div className="lg:hidden">
-            <Sheet open={menuOpen} onOpenChange={toggleMenu}>
-              <SheetTrigger asChild>
-                <Button size="sm" variant="secondary" onClick={toggleMenu}>
-                  {menuOpen ? <></> : <Menu />}
-                </Button>
-              </SheetTrigger>
-              <SheetContent>
-                <div>
-                  <Link href="/">
-                    <Image src="/logos.svg" width={175} height={100} alt="logo" />
-                  </Link>
-                  <div className="flex mt-10 flex-col gap-6 text-lg font-semibold">
-                    <NavLink href="/">Home</NavLink>
-                    <NavLink href="/">Swap Token</NavLink>
-                    <NavLink href="/">Team</NavLink>
+          <div className="flex items-center gap-10">
+            {/* Display menu links on large screens */}
+            <div className="hidden lg:flex gap-10 text-lg font-semibold">
+              <NavLink href="/">About</NavLink>
+              <NavLink href="/">Blog</NavLink>
+              <NavLink href="/">Contact</NavLink>
+            </div>
+
+            {/* Display Launch dApp button on large screens */}
+            <Button className="bg-red-200 rounded-xl hidden lg:block text-black font-bold">
+              Launch dApp
+            </Button>
+
+            {/* =========================== */}
+            {/* DISPLAY THE MENU FOR MOBILE */}
+            {/* =========================== */}
+            <div className="lg:hidden">
+              <Sheet open={menuOpen} onOpenChange={toggleMenu}>
+                <SheetTrigger asChild>
+                  <Button size="sm" variant="secondary" onClick={toggleMenu}>
+                    {menuOpen ? <></> : <Menu />}
+                  </Button>
+                </SheetTrigger>
+                <SheetContent>
+                  <div>
+                    <Link href="/">
+                      <Image
+                        src="/image.png"
+                        width={175}
+                        height={100}
+                        alt="logo"
+                      />
+                    </Link>
+                    <div className="flex mt-10 flex-col gap-6 text-lg font-semibold text-white">
+                      <NavLink href="/">About</NavLink>
+                      <NavLink href="/">Blog</NavLink>
+                      <NavLink href="/">Contact</NavLink>
+                    </div>
+                    <Link href={"/app"} className="mt-10 flex gap-6">
+                      <Button className="bg-red-200 rounded-xl text-black font-bold">
+                        Launch dApp
+                      </Button>
+                    </Link>
                   </div>
-                  <div className="mt-10 flex gap-6">
-                    <ConnectWalletButton />
-                  </div>
-                </div>
-              </SheetContent>
-            </Sheet>
+                </SheetContent>
+              </Sheet>
+            </div>
           </div>
         </div>
       </div>
