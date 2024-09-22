b17060e528f83d32c304f0e54abd471dd7f68875
tusharpamnani <tusharpamnani1@gmail.com>
2024-09-22 13:08:25 +0530

    feat: added payments page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,7 +1,8 @@
-import Hero from "@/components/Hero";
+import Hero from "../components/Hero";
 import Header from "@/components/Navbar";
 import React from "react";
 const Page = () => {
+  console.log(process.env.NEXT_PUBLIC_HELIUS_API_KEY)
   return (
     <div className="bg-[#021524]">
       <Header />
